a802deda75b3cb86541c8ad8746a6b1a27a0a416
Aksel Sloan <akselsloan@gmail.com>
2025-01-21 10:18:04 -1000

    Setting up inference script.

---
+++ inference_local_dataset.py
@@ -0,0 +1,11 @@
+'''
+File:   interence_local_dataset.py
+
+Spec:   This script it used to inference a large, local dataset
+        and output the training results into ______ forma.
+        Results are stored into unsupervised_annotations folder. 
+'''
+
+from ultralytics import YOLO
+
+
